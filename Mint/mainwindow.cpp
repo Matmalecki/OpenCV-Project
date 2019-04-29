470f7e6b2ff13abf49e9521bc1b03ac99870c6d6
Mateusz Małecki <mmateusz314@gmail.com>
2019-04-29 17:48:24 +0100

    Add files via upload

---
+++ Mint/mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QTimer>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    setUpManager();
+}
+
+MainWindow::~MainWindow()
+{
+    thread->quit();
+    while(!thread->isFinished());
+    delete thread;
+    delete ui;
+}
+
+void MainWindow::setUpManager()
+{
+    thread = new QThread();
+    OpencvManager * manager = new OpencvManager();
+    QTimer * managerTrigger = new QTimer();
+    managerTrigger->setInterval(1);
+
+    connect(managerTrigger, SIGNAL(timeout()), manager, SLOT(receiveGrabFrame()));
+    connect(this, SIGNAL(sendSetup(int)), manager, SLOT(receiveSetup(int)));
+    connect(this, SIGNAL(sendToggleStream()), manager, SLOT(receiveToggleStream()));
+    connect(ui->playButton, SIGNAL(clicked(bool)), this, SLOT(receiveToggleStream()));
+    connect(manager, SIGNAL(sendFrame(QImage)), this, SLOT(receiveFrame(QImage)));
+
+    managerTrigger->start();
+    manager-> moveToThread(thread);
+    managerTrigger->moveToThread(thread);
+
+    thread->start();
+
+    emit sendSetup(0);
+}
+
+void MainWindow::receiveFrame(QImage frame)
+{
+    ui->videoField->setPixmap(QPixmap::fromImage(frame));
+}
+
+void MainWindow::receiveToggleStream()
+{
+    if (!ui->playButton->text().compare(">")) ui->playButton->setText("||");
+    else ui->playButton->setText(">");
+
+    emit sendToggleStream();
+}
---
+++ Mint/opencvmanager.cpp
@@ -0,0 +1,74 @@
+#include "opencvmanager.h"
+
+using namespace cv;
+
+OpencvManager::OpencvManager(QObject * parent) : QObject (parent), status(false), toggleStream(false),
+  binaryThresholdEnable(false), binaryThreshold(127)
+{
+
+    cap = new cv::VideoCapture();
+
+
+
+}
+
+OpencvManager::~OpencvManager()
+{
+    if (cap->isOpened()) cap->release();
+    delete cap;
+}
+
+void OpencvManager::checkIfDeviceAlreadyOpened(const int device)
+{
+    if (cap->isOpened()) cap->release();
+    cap->open(device);
+
+}
+
+
+void OpencvManager::receiveGrabFrame()
+{
+    if (!toggleStream)
+    {
+        return;
+    }
+    cap->read(image);
+    if (image.empty()) return;
+
+    process();
+
+    QImage output((const unsigned char *)processedImage.data, processedImage.cols, processedImage.rows, QImage::Format_Indexed8);
+
+
+    emit sendFrame(output);
+}
+
+void OpencvManager::process()
+{
+     cvtColor(image, processedImage, CV_BGR2GRAY);
+}
+
+void OpencvManager::receiveSetup(const int device)
+{
+    checkIfDeviceAlreadyOpened(device);
+    if (!cap->isOpened())
+    {
+        status = false;
+        return;
+    }
+    status = true;
+}
+
+void OpencvManager::receiveToggleStream()
+{
+    toggleStream = !toggleStream;
+}
+
+void OpencvManager::receiveEnableBinaryThreshold(){
+    binaryThresholdEnable = !binaryThresholdEnable;
+}
+
+void OpencvManager::receiveBinaryThreshold(int threshold)
+{
+    binaryThreshold = threshold;
+}
